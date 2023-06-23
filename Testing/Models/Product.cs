9dfe3837850f4b84adeb427973bb1d9879a2700f
Joseph LaVia <josephdlavia@gmail.com>
2023-06-23 14:01:30 -0500

    ASPCreateXercise

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -11,8 +13,10 @@
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
-                         
+        public IEnumerable<Category> Categories { get; set; }
 
-                    
+
+
+
     }
 }
