8b897c847f30266a0d491b759c5fab39b57983f1
Joseph LaVia <josephdlavia@gmail.com>
2023-06-23 10:03:06 -0500

    ASP.netXercise

---
+++ Testing/Controllers/ProductController1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+
+
+    public class ProductController : Controller
+    {
+
+        private readonly IProductRepo repo;
+
+        public ProductController(IProductRepo repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View(products);
+        }
+    }
+}
+
---
+++ Testing/ProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
